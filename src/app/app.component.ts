1247e79e6bafb7134255dbfd071b822f10b0868e
Stefan von Niederhäusern <svn@nexplore.ch>
2019-03-04 11:38:45 +0100

    add contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class ContactsAppComponent {
-  title = 'Angular Master Class';
+  contact: Contact = {
+    id: 6,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/6.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  }
 }
